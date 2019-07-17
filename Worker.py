66651573cbf33989bc2adf2dbb3bd6ca73b84c41
Tobias <tobias@deep.server.verbraucherportal.check24-int.de>
2019-07-17 15:00:55 +0200

    Working version of the ocr_worker

--- Worker.py
+++ Worker.py
@@ -3,6 +3,8 @@ from Utils import load_rabbit_config
 from ImageConverter import ImageConverter, FileFormat
 from Model.OcrTask import OcrTaskModel, TaskState
 import logging
+import threading
+import functools
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
@@ -10,10 +12,21 @@ LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
+def akk_message(channel, delivery_tag):
+    if channel.is_open:
+        channel.basic_ack(delivery_tag)
+    else:
+        pass
 
-def handle_task(ch, method, properties, body):
-    print("Getting a new task!")
+def handle_task(connection, channel, delivery_tag, body):
     task_id = body.decode("utf-8")
+    OcrTaskModel.change_status(task_id, TaskState.PENDING.value)
+    
+    print("Getting a new task!")
+    thread_id = threading.get_ident()
+    fmt1 = 'Thread id: {} Delivery tag: {} Message body: {}'
+    LOGGER.info(fmt1.format(thread_id, delivery_tag, body))
+    
     converter = ImageConverter(task_id , input_format=FileFormat.PDF)
     print("Initializing Converter")
     text = converter.read_text_from_image()
@@ -22,6 +35,14 @@ def handle_task(ch, method, properties, body):
     print("Text saved")
     OcrTaskModel.change_status(task_id, TaskState.DONE.value)
     print("Status changed")
+    
+def on_message(channel, method_frame, header_frame, body, args):
+    (connection, threads) = args
+    delivery_tag = method_frame.delivery_tag
+    t = threading.Thread(target=handle_task, args=(connection, channel, delivery_tag, body))
+    t.start()
+    threads.append(t)
+    
 
 class Worker():
     def __init__(self):
@@ -41,9 +62,21 @@ class Worker():
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
         channel.queue_declare(queue=queue)
-        channel.basic_consume(queue=queue, on_message_callback=handle_task, auto_ack=True)
-        channel.start_consuming()
         
+        threads = []
+        on_message_callback = functools.partial(on_message, args=(connection, threads))
+        channel.basic_consume(queue=queue, on_message_callback=on_message_callback)
+        
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+
+        # Wait for all to complete
+        for thread in threads:
+            thread.join()
+
+        connection.close()
         
 
     
